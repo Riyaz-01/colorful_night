eac40b584a7ac951ee50110d7edca730ea3aa5d1
Riyaz Ahmad <riyazansari2017@gmail.com>
2024-01-06 17:10:26 +0530

    click sound

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import './Button.scss';
+
+import clickSound from '../../assets/sounds/click.mp3';
 
 const Button = ({
 	children = <></>,
@@ -8,18 +10,24 @@ const Button = ({
 	...props
 }) => {
 	const [animate, setAnimate] = useState(false);
+	const audioRef = useRef(null);
+
 	const handleClick = () => {
 		setAnimate(true);
+		audioRef.current.play();
 		setTimeout(() => {
 			onClick();
 			setAnimate(false);
 		}, delay);
 	};
+
 	return (
 		<div className='btn-cont' {...props} onClick={handleClick}>
 			<button className={'custom-button btn ' + (animate && 'fade')}>
 				{children}
 			</button>
+
+			<audio controls src={clickSound} ref={audioRef} id='click' />
 		</div>
 	);
 };
